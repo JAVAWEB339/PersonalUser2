d607e3b823cdf4a3cc012b1548f77100b619248e
JAVAWEB339 <65803861+JAVAWEB339@users.noreply.github.com>
2021-04-13 16:27:09 +0800

    Delete js directory

--- js/login.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    var success = document.getElementsByClassName("login");
-    success[0].onsubmit = function () {
-        var value1 = document.getElementById("username").value;
-        var password1 = document.getElementById("password").value;
-        var userinfo = {user : value1, password : password1};
-        sessionStorage.setItem("UserInfo:", JSON.stringify(userinfo));
-    }
-}
